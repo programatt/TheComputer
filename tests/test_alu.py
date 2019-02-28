153cb25a764ab38da5363181770a1324020685ef
Matt Phillips <matt@phillipssoftware.co>
2019-02-28 14:00:10 -0600

    PEP8 ignore only E241

--- tests/test_alu.py
+++ tests/test_alu.py
@@ -13,14 +13,14 @@ def b2b(lst):
 def test_simplealu_single_bit_alu_truth_table():
     truth_table = b2b([
         #A, B, CI, CO, S, A&B, A|B, A, ~B
-        [0, 0, 0,  0,  0,  0,   0,  0,  1],
-        [0, 0, 1,  0,  1,  0,   0,  0,  1],
-        [0, 1, 0,  0,  1,  0,   1,  0,  0],
-        [0, 1, 1,  1,  0,  0,   1,  0,  0],
-        [1, 0, 0,  0,  1,  0,   1,  1,  1],
-        [1, 0, 1,  1,  0,  0,   1,  1,  1],
-        [1, 1, 0,  1,  0,  1,   1,  1,  0],
-        [1, 1, 1,  1,  1,  1,   1,  1,  0]
+        [0, 0, 0,  0,  0,  0,   0,  0,  1],  # flake8: noqa
+        [0, 0, 1,  0,  1,  0,   0,  0,  1],  # flake8: noqa
+        [0, 1, 0,  0,  1,  0,   1,  0,  0],  # flake8: noqa
+        [0, 1, 1,  1,  0,  0,   1,  0,  0],  # flake8: noqa
+        [1, 0, 0,  0,  1,  0,   1,  1,  1],  # flake8: noqa
+        [1, 0, 1,  1,  0,  0,   1,  1,  1],  # flake8: noqa
+        [1, 1, 0,  1,  0,  1,   1,  1,  0],  # flake8: noqa
+        [1, 1, 1,  1,  1,  1,   1,  1,  0]  # flake8: noqa
     ])
     for row in truth_table:
         (a, b, cin) = tuple(row[:3])
@@ -34,20 +34,25 @@ def test_simplealu_multiplex_3in_5out():
     0010 = A or B
     0011 = A
     0100 = Not B
-    :return:
+    :return: out
     """
     truth_table = [
         #cw0, cw1, cw2, s, a&b, a|b, a, ~b, out  output column
-        [0,   0,   0,  1,  0,   0,  0,  0,  1],  # s
-        [0,   0,   0,  0,  0,   0,  0,  0,  0],  # s
-        [0,   0,   1,  0,  1,   0,  0,  0,  1],  # a&b
-        [0,   0,   1,  0,  0,   0,  0,  0,  0],  # a&b
-        [0,   1,   0,  0,  0,   1,  0,  0,  1],  # a|b
-        [0,   1,   0,  0,  0,   0,  0,  0,  0],  # a|b
-        [0,   1,   1,  0,  0,   0,  1,  0,  1],  # a
-        [0,   1,   1,  0,  0,   0,  0,  0,  0],  # a
-        [1,   0,   0,  0,  0,   0,  0,  1,  1],  # ~b
-        [1,   0,   0,  0,  0,   0,  0,  0,  0],  # ~b
+        # out = s
+        [0,   0,   0,  1,  0,   0,  0,  0,  1],  # flake8: noqa
+        [0,   0,   0,  0,  0,   0,  0,  0,  0],  # flake8: noqa
+        # out = a&b
+        [0,   0,   1,  0,  1,   0,  0,  0,  1],  # flake8: noqa
+        [0,   0,   1,  0,  0,   0,  0,  0,  0],  # flake8: noqa
+        # out = a|b
+        [0,   1,   0,  0,  0,   1,  0,  0,  1],  # flake8: noqa
+        [0,   1,   0,  0,  0,   0,  0,  0,  0],  # flake8: noqa
+        # out = a
+        [0,   1,   1,  0,  0,   0,  1,  0,  1],  # flake8: noqa
+        [0,   1,   1,  0,  0,   0,  0,  0,  0],  # flake8: noqa
+        # out = ~b
+        [1,   0,   0,  0,  0,   0,  0,  1,  1],  # flake8: noqa
+        [1,   0,   0,  0,  0,   0,  0,  0,  0],  # flake8: noqa
     ]
     for row in truth_table:
         (s, andb, aorb, a, notb, out) = tuple(row[3:])
@@ -65,34 +70,34 @@ def test_simplealu_multi_bit_alu_add():
 
 def test_simplealu_multi_bit_alu_aandb():
     control_word = BoolArray(bits=SimpleALU.AND_OPERATION)
-    a    = BoolArray(bits=[True, False, True,  False])
-    b    = BoolArray(bits=[True, False, False, True])
-    andb = BoolArray(bits=[True, False, False, False])
+    a    = BoolArray(bits=[True, False, True,  False])  # flake8: noqa
+    b    = BoolArray(bits=[True, False, False, True])  # flake8: noqa
+    andb = BoolArray(bits=[True, False, False, False])  # flake8: noqa
     output = SimpleALU.multi_bit_alu(a, b, control_word)
     assert(output == andb)
 
 
 def test_simplealu_multi_bit_alu_aorb():
     control_word = BoolArray(bits=SimpleALU.OR_OPERATION)
-    a    = BoolArray(bits=[True, False, True,  False])
-    b    = BoolArray(bits=[True, False, False, True])
-    aorb = BoolArray(bits=[True, False, True,  True])
+    a    = BoolArray(bits=[True, False, True,  False])  # flake8: noqa
+    b    = BoolArray(bits=[True, False, False, True])  # flake8: noqa
+    aorb = BoolArray(bits=[True, False, True,  True])  # flake8: noqa
     output = SimpleALU.multi_bit_alu(a, b, control_word)
     assert(output == aorb)
 
 
 def test_simplealu_multi_bit_alu_a():
     control_word = BoolArray(bits=SimpleALU.A_OPERATION)
-    a = BoolArray(bits=[True, False, True,  False])
-    b = BoolArray(bits=[True, False, False, True])
+    a = BoolArray(bits=[True, False, True,  False])  # flake8: noqa
+    b = BoolArray(bits=[True, False, False, True])  # flake8: noqa
     output = SimpleALU.multi_bit_alu(a, b, control_word)
     assert(output == a)
 
 
 def test_simplealu_multi_bit_alu_not_b():
     control_word = BoolArray(bits=SimpleALU.NOTB_OPERATION)
-    a    = BoolArray(bits=[False, False, False, False])
-    b    = BoolArray(bits=[True,  False, True,  False])
-    notb = BoolArray(bits=[False, True,  False, True])
+    a    = BoolArray(bits=[False, False, False, False])  # flake8: noqa
+    b    = BoolArray(bits=[True,  False, True,  False])  # flake8: noqa
+    notb = BoolArray(bits=[False, True,  False, True])  # flake8: noqa
     output = SimpleALU.multi_bit_alu(a, b, control_word)
     assert(output == notb)
