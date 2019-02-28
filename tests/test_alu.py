958aeac9af0c10396cdd8d4b3650642ba23ded9f
Matt Phillips <matt@phillipssoftware.co>
2019-02-28 13:36:57 -0600

    cleanup tests for readability

--- tests/test_alu.py
+++ tests/test_alu.py
@@ -12,7 +12,7 @@ def b2b(lst):
 
 def test_simplealu_single_bit_alu_truth_table():
     truth_table = b2b([
-        # A, B, CI, CO, S, A&B, A|B, A, ~B
+        #A, B, CI, CO, S, A&B, A|B, A, ~B
         [0, 0, 0,  0,  0,  0,   0,  0,  1],
         [0, 0, 1,  0,  1,  0,   0,  0,  1],
         [0, 1, 0,  0,  1,  0,   1,  0,  0],
@@ -37,17 +37,17 @@ def test_simplealu_multiplex_3in_5out():
     :return:
     """
     truth_table = [
-        # cw0, cw1, cw2, s, a&b, a|b, a, ~b, out   output column
-        [0,   0,   0,  1,  0,   0,  0, 0,   1],  # s
-        [0,   0,   0,  0,  0,   0,  0, 0,   0],  # s
-        [0,   0,   1,  0,  1,   0,  0, 0,   1],  # a&b
-        [0,   0,   1,  0,  0,   0,  0, 0,   0],  # a&b
-        [0,   1,   0,  0,  0,   1,  0, 0,   1],  # a|b
-        [0,   1,   0,  0,  0,   0,  0, 0,   0],  # a|b
-        [0,   1,   1,  0,  0,   0,  1, 0,   1],  # a
-        [0,   1,   1,  0,  0,   0,  0, 0,   0],  # a
-        [1,   0,   0,  0,  0,   0,  0, 1,   1],  # ~b
-        [1,   0,   0,  0,  0,   0,  0, 0,   0],  # ~b
+        #cw0, cw1, cw2, s, a&b, a|b, a, ~b, out  output column
+        [0,   0,   0,  1,  0,   0,  0,  0,  1],  # s
+        [0,   0,   0,  0,  0,   0,  0,  0,  0],  # s
+        [0,   0,   1,  0,  1,   0,  0,  0,  1],  # a&b
+        [0,   0,   1,  0,  0,   0,  0,  0,  0],  # a&b
+        [0,   1,   0,  0,  0,   1,  0,  0,  1],  # a|b
+        [0,   1,   0,  0,  0,   0,  0,  0,  0],  # a|b
+        [0,   1,   1,  0,  0,   0,  1,  0,  1],  # a
+        [0,   1,   1,  0,  0,   0,  0,  0,  0],  # a
+        [1,   0,   0,  0,  0,   0,  0,  1,  1],  # ~b
+        [1,   0,   0,  0,  0,   0,  0,  0,  0],  # ~b
     ]
     for row in truth_table:
         (s, andb, aorb, a, notb, out) = tuple(row[3:])
@@ -65,35 +65,34 @@ def test_simplealu_multi_bit_alu_add():
 
 def test_simplealu_multi_bit_alu_aandb():
     control_word = BoolArray(bits=SimpleALU.AND_OPERATION)
-    a = BoolArray(bits=[True, False, True, False])
-    b = BoolArray(bits=[True, False, False, True])
-    # Assert Result = b (implement eq method on BoolArray)
+    a    = BoolArray(bits=[True, False, True,  False])
+    b    = BoolArray(bits=[True, False, False, True])
+    andb = BoolArray(bits=[True, False, False, False])
     output = SimpleALU.multi_bit_alu(a, b, control_word)
-    assert(output == [True, False, False, False])
+    assert(output == andb)
 
 
 def test_simplealu_multi_bit_alu_aorb():
     control_word = BoolArray(bits=SimpleALU.OR_OPERATION)
-    a = BoolArray(bits=[True, False, True, False])
-    b = BoolArray(bits=[True, False, False, True])
-    # Assert Result = b (implement eq method on BoolArray)
+    a    = BoolArray(bits=[True, False, True,  False])
+    b    = BoolArray(bits=[True, False, False, True])
+    aorb = BoolArray(bits=[True, False, True,  True])
     output = SimpleALU.multi_bit_alu(a, b, control_word)
-    assert(output == [True, False, True, True])
+    assert(output == aorb)
 
 
 def test_simplealu_multi_bit_alu_a():
     control_word = BoolArray(bits=SimpleALU.A_OPERATION)
-    a = BoolArray(bits=[True, False, True, False])
+    a = BoolArray(bits=[True, False, True,  False])
     b = BoolArray(bits=[True, False, False, True])
-    # Assert Result = b (implement eq method on BoolArray)
     output = SimpleALU.multi_bit_alu(a, b, control_word)
     assert(output == a)
 
 
 def test_simplealu_multi_bit_alu_not_b():
     control_word = BoolArray(bits=SimpleALU.NOTB_OPERATION)
-    a = BoolArray(bits=[False, False, False, False])
-    b = BoolArray(bits=[True, False, True, False])
-    # Assert Result = b (implement eq method on BoolArray)
+    a    = BoolArray(bits=[False, False, False, False])
+    b    = BoolArray(bits=[True,  False, True,  False])
+    notb = BoolArray(bits=[False, True,  False, True])
     output = SimpleALU.multi_bit_alu(a, b, control_word)
-    assert(output == BoolArray(bits=[False, True, False, True]))
+    assert(output == notb)
